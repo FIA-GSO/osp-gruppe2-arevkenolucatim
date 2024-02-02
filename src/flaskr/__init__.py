56f90e2a30b676bae3edbe94caf5a733b5245692
fi107lucamienert <lucatorben.m@gso.schule.koeln>
2024-02-02 10:54:07 +0100

    Weitere Flaks struktur aufgebaut

--- src/flaskr/__init__.py
+++ src/flaskr/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from . import db
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -8,18 +9,10 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    db.init_app(app)
 
     return app
