4943e9d17e1c7c40e833cbb9e39959840791cb7b
fi107lucamienert <lucatorben.m@gso.schule.koeln>
2024-02-02 13:20:33 +0100

    Fehlende Routen hinzugefügt

--- src/flaskr/internal.py
+++ src/flaskr/internal.py
@@ -8,3 +8,13 @@ bp = Blueprint('internal', __name__, url_prefix='/')
 @bp.route('/company', methods=['GET', 'POST'])
 def company_view():
     return render_template('internal/companyView.html')
+
+
+@bp.route('/requestConfirm', methods=['GET'])
+def request_confirm():
+    return render_template('internal/requestConfirm.html')
+
+
+@bp.route('/organisation', methods=['GET', 'POST'])
+def organisation_view():
+    return render_template('internal/organisationView.html')
