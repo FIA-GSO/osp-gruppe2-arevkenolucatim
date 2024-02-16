1dd6cabbcad0a7df1162f1912af615bbc1451f72
fi107lucamienert <lucatorben.m@gso.schule.koeln>
2024-02-16 08:21:33 +0100

    Encoding bug

--- src/flaskr/public.py
+++ src/flaskr/public.py
@@ -3,7 +3,7 @@ from flask import (Blueprint, flash, g, redirect, render_template, request, sess
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
-bp = Blueprint('publić', __name__, url_prefix='/')
+bp = Blueprint('public', __name__, url_prefix='/')
 
 @bp.route('/faq', methods=['GET'])
 def faq():
