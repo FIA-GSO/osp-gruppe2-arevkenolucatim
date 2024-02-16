86e60f10355fdb360d444e6650e7ac9d5f9ac514
Keno Marciniak <kenoluca.m@gso.schule.koeln>
2024-02-16 10:16:39 +0100

    Erstellung des Datenbankschemas

--- src/flaskr/schema.sql
+++ src/flaskr/schema.sql
@@ -1,3 +1,4 @@
+/*
 DROP TABLE IF EXISTS faq;
 
 CREATE TABLE faq (
@@ -5,3 +6,33 @@ CREATE TABLE faq (
     question  TEXT NOT NULL,
     answer    TEXT NOT NULL
 );
+*/
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS User (
+	ID	        INTEGER NOT NULL,
+	Company	    TEXT NOT NULL,
+	Password	TEXT NOT NULL,
+	Email	    TEXT NOT NULL,
+	Contact	    TEXT,
+	Telephone	TEXT,
+	PRIMARY KEY(ID AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS Faq (
+	ID	        INTEGER NOT NULL,
+	Question	TEXT,
+	Answer	    TEXT,
+	PRIMARY KEY(ID AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS Application (
+	ID	            INTEGER NOT NULL,
+	UserID	        INTEGER NOT NULL,
+	Days	        INTEGER NOT NULL,
+	Remarks	        TEXT,
+	Table	        INTEGER,
+	Chair	        INTEGER,
+	LectureTopic	TEXT,
+	LectureLenght   INTEGER,
+	Status	        INTEGER NOT NULL,
+	PRIMARY KEY(ID,UserID)
+);
+COMMIT;
